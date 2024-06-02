cbb344b8e9582ca40bea8923a9bbc979c1be4cad
Kevin Michalewicz <kmichalewicz@fi.uba.ar>
2024-06-02 17:31:50 +0100

    Release 1.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except(IOError, ImportError):
 
 setup(
     name='ANTIPASTI',
-    version='1.1',
+    version='1.2',
     author='Kevin Michalewicz',
     author_email='k.michalewicz22@imperial.ac.uk',
     description='Deep Learning model that predicts the binding affinity of antibodies from their three-dimensional structure.',
